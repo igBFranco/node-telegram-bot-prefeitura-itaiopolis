299cd3656ecaf4dade60bf6c69542d661aa77e7d
igBFranco <igor.franco@aluno.unc.br>
2022-09-05 15:15:12 -0300

    Adicionando Setores, Link dos Canais Oficiais e Noticias

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ bot.start((ctx) => {
                 ]
             ],
             resize_keyboard: true,
-            one_time_keyboard: true
         }
     })
 });
@@ -50,9 +49,37 @@ bot.on('text', ctx => {
                         {text: "Bloco de Notas"},
                         {text: "Casa da Cultura"},
                     ],
+                    [
+                        {text: "Saúde"},
+                        {text: "Vigilância Epidemiológica"},
+                        {text: "Vigilância Sanitária"},
+                    ],
                 ],
                 resize_keyboard: true,
-                one_time_keyboard: true
+            }
+        })
+    }
+    if (ctx.message.text === "Últimas Notícias") {
+        bot.telegram.sendMessage(ctx.chat.id, `Acesse o Portal Municipal para acompanhar as últimas notícias`, {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {text: "Portal Municipal", url: "https://www.itaiopolis.sc.gov.br/"},
+                    ]
+                ],
+            }
+        })
+    }
+    if (ctx.message.text === "Canais Oficiais") {
+        bot.telegram.sendMessage(ctx.chat.id, `Acompanhe a Prefeitura em seus Canais Oficiais!`, {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {text: "Portal Municipal", url: "https://www.itaiopolis.sc.gov.br/"},
+                        {text: "Instagram", url: "https://www.instagram.com/prefeitura_itaiopolis/"},
+                        {text: "Facebook", url: "https://www.facebook.com/prefeituraitaoficial/"},
+                    ]
+                ],
             }
         })
     }
@@ -75,7 +102,8 @@ bot.on('text', ctx => {
         bot.telegram.sendMessage(ctx.chat.id, `📧 Email: juridico@itaiopolis.sc.gov.br`, {});
     }else if (ctx.message.text === "Recursos Humanos") {
         bot.telegram.sendMessage(ctx.chat.id, `📞 Telefone: (47)3652-2211`, {});
-        bot.telegram.sendMessage(ctx.chat.id, `📧 Email: rh.prefeitura.itaiopolis@gmail.com`, {});
+        bot.telegram.sendMessage(ctx.chat.id, `📧 Email: rh.prefeitura.itaiopolis@gmail.com
+        segurancatrabalho@itaiopolis.sc.gov.br`, {});
     }else if (ctx.message.text === "Tesouraria") {
         bot.telegram.sendMessage(ctx.chat.id, `📞 Telefone: (47)3652-2211`, {});
         bot.telegram.sendMessage(ctx.chat.id, `📧 Email: tesouraria@itaiopolis.sc.gov.br
@@ -96,10 +124,30 @@ bot.on('text', ctx => {
         bot.telegram.sendMessage(ctx.chat.id, `📞 Telefone: (47)3652-2211`, {});
         bot.telegram.sendMessage(ctx.chat.id, `📧 Email: licitacao@itaiopolis.sc.gov.br
             cotacao.itaiopolis@gmail.com`, {});
+    }else if (ctx.message.text === "Obras") {
+        bot.telegram.sendMessage(ctx.chat.id, `📞 Telefone: (47)3652-2211`, {});
+        bot.telegram.sendMessage(ctx.chat.id, `📧 Email: agronegocio@itaiopolis.sc.gov.br`, {});
     }else if (ctx.message.text === "Agronegócio") {
         bot.telegram.sendMessage(ctx.chat.id, `📞 Telefone: (47)3652-2211`, {});
         bot.telegram.sendMessage(ctx.chat.id, `📧 Email: agronegocio@itaiopolis.sc.gov.br`, {});
+    }else if (ctx.message.text === "Bloco de Notas") {
+        bot.telegram.sendMessage(ctx.chat.id, `📞 Telefone: (47)3652-2844`, {});
+        bot.telegram.sendMessage(ctx.chat.id, `📧 Email: agronegocio@itaiopolis.sc.gov.br`, {});
+    }else if (ctx.message.text === "Casa da Cultura") {
+        bot.telegram.sendMessage(ctx.chat.id, `📞 Telefone: (47)3652-1306`, {});
+        bot.telegram.sendMessage(ctx.chat.id, `📧 Email: cultura@itaiopolis.sc.gov.br
+        turismo@itaiopolis.sc.gov.br`, {});
+    }else if (ctx.message.text === "Saúde") {
+        bot.telegram.sendMessage(ctx.chat.id, `📞 Telefone: (47)3652-1893`, {});
+        bot.telegram.sendMessage(ctx.chat.id, `📧 Email: saude@itaiopolis.sc.gov.br`, {});
+    }else if (ctx.message.text === "Vigilância Epidemiológica") {
+        bot.telegram.sendMessage(ctx.chat.id, `📞 Telefone: (47)3652-2760`, {});
+        bot.telegram.sendMessage(ctx.chat.id, `📧 Email: vigilancia@itaiopolis.sc.gov.br`, {});
+    }else if (ctx.message.text === "Vigilância Sanitária") {
+        bot.telegram.sendMessage(ctx.chat.id, `📞 Telefone: (47)3652-1492`, {});
+        bot.telegram.sendMessage(ctx.chat.id, `📧 Email: vigilancia@itaiopolis.sc.gov.br`, {});
     }
+
 });
 
 bot.hears('oi', (ctx) => ctx.reply(`Olá! ${ctx.chat.first_name}`))
